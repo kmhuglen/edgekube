9f0313e00fd3d6c46e2e9c5a6d2254698ae3475e
Knut Magne Huglen <kmhuglen@outlook.com>
2022-04-23 19:42:56 +0200

    write-host

--- DockerVMFeatures.ps1
+++ DockerVMFeatures.ps1
@@ -1,22 +1,16 @@
 Function Get-UbuntuCloudImage {
     [CmdletBinding()]
     param(
-        [string]$OutputPath,
-        [switch]$Previous
+        [string]$OutputPath
     )
     
     $ErrorActionPreference = 'Stop'
     
     # Enables TLS 1.2
     [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
-    
-    if ($Previous) {
-        $urlRoot = 'https://cloud-images.ubuntu.com/releases/bionic/release/'
-        $urlFile = 'ubuntu-18.04-server-cloudimg-amd64.img'
-    } else {
-        $urlRoot = 'https://cloud-images.ubuntu.com/releases/focal/release/'
-        $urlFile = 'ubuntu-20.04-server-cloudimg-amd64.img'
-    }
+ 
+    $urlRoot = 'https://cloud-images.ubuntu.com/releases/focal/release/'
+    $urlFile = 'ubuntu-20.04-server-cloudimg-amd64.img'
     
     $url = "$urlRoot/$urlFile"
             
@@ -27,14 +21,14 @@ Function Get-UbuntuCloudImage {
     $imgFile = Join-Path $OutputPath $urlFile
     
     if ([System.IO.File]::Exists($imgFile)) {
-        Write-Verbose "File '$imgFile' already exists. Nothing to do."
+        Write-Host "File '$imgFile' already exists. Nothing to do."
     } else {
-        Write-Verbose "Downloading file '$imgFile'..."
+        Write-Host "Downloading file '$imgFile'..."
     
         $client = New-Object System.Net.WebClient
         $client.DownloadFile($url, $imgFile)
     
-        Write-Verbose "Checking file integrity..."
+        Write-Host "Checking file integrity..."
         $sha1Hash = Get-FileHash $imgFile -Algorithm SHA256
         $allHashs = $client.DownloadString("$urlRoot/SHA256SUMS")
         $m = [regex]::Matches($allHashs, "(?<Hash>\w{64})\s\*$urlFile")
