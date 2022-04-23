8daa1b996daeb3ef0dc5cc1989ecb4ffbdc7caa0
Knut Magne Huglen <kmhuglen@outlook.com>
2022-04-23 20:40:59 +0200

    tools update

--- Get-Tools.ps1
+++ Get-Tools.ps1
@@ -1,3 +1,4 @@
 New-Item -ItemType Directory -Path tools
 Invoke-WebRequest -UseBasicParsing -Uri https://cloudbase.it/downloads/qemu-img-win-x64-2_3_0.zip -OutFile tools\qemu-img-win-x64-2_3_0.zip
 Expand-Archive -Path tools\qemu-img-win-x64-2_3_0.zip -DestinationPath tools\qemu-img-win-x64
+Invoke-WebRequest -UseBasicParsing -Uri https://github.com/fdcastel/Hyper-V-Automation/raw/master/tools/oscdimg.exe -OutFile tools\oscdimg.exe
