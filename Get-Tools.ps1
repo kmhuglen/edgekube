127c02a8f1c9a100f2fbf077b7e060959318bca9
Knut Magne Huglen <kmhuglen@outlook.com>
2022-04-25 10:09:53 +0200

    add if to get-tools

--- Get-Tools.ps1
+++ Get-Tools.ps1
@@ -1,5 +1,17 @@
 # Download Tools
-New-Item -ItemType Directory -Path tools
-Invoke-WebRequest -UseBasicParsing -Uri https://cloudbase.it/downloads/qemu-img-win-x64-2_3_0.zip -OutFile tools\qemu-img-win-x64-2_3_0.zip
-Expand-Archive -Path tools\qemu-img-win-x64-2_3_0.zip -DestinationPath tools\qemu-img-win-x64
-Invoke-WebRequest -UseBasicParsing -Uri https://github.com/fdcastel/Hyper-V-Automation/raw/master/tools/oscdimg.exe -OutFile tools\oscdimg.exe
+If (Test-Path -Path ".\tools") {
+    # Tools folder exist
+} else {
+    New-Item -ItemType Directory -Path tools
+}
+If (Test-Path -Path ".\tools\qemu-img-win-x64") {
+    # qemu-img-win-x64 folder exist
+} else {
+    Invoke-WebRequest -UseBasicParsing -Uri https://cloudbase.it/downloads/qemu-img-win-x64-2_3_0.zip -OutFile tools\qemu-img-win-x64-2_3_0.zip
+    Expand-Archive -Path tools\qemu-img-win-x64-2_3_0.zip -DestinationPath tools\qemu-img-win-x64
+}
+if (Test-Path -Path ".\tools\oscdimg.exe") {
+    # oscdimg.exe exist
+} else {
+    Invoke-WebRequest -UseBasicParsing -Uri https://github.com/fdcastel/Hyper-V-Automation/raw/master/tools/oscdimg.exe -OutFile tools\oscdimg.exe
+}
--- README.md
+++ README.md
@@ -3,13 +3,8 @@
 Run the following commands to set up edgekube on a Hyper-V server
 
 ```PowerShell
-# Download the repository and expand it to the current location
-Invoke-WebRequest -UseBasicParsing -Uri https://github.com/kmhuglen/edgekube/archive/refs/heads/main.zip -OutFile docker1.zip
-Expand-Archive -Path docker1.zip .\
-Set-Location .\edgekube-main
-
-# Download needed tools
-.\Get-Tools.ps1
+# Download repository and tools
+Invoke-WebRequest -UseBasicParsing -Uri https://github.com/kmhuglen/edgekube/archive/refs/heads/main.zip -OutFile edgekube-main.zip ; Expand-Archive -Path edgekube-main.zip .\ -Force ; Remove-Item .\edgekube-main.zip ; Set-Location .\edgekube-main ; .\Get-Tools.ps1
 
 # Deploy the VM
 .\Deploy-UbuntuVM.ps1 -ConfigFile .\config-docker1
